3f8ada8d8b5d29d9e0d380345f94ba6a1882d53d
Matt Storey <storeyms2010@gmail.com>
2020-03-05 20:16:35 -0600

    added product table w/ adjustments to formatting with CSS

---
+++ ASPNET/Models/product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNET.Models
+{
+    public class product
+    {
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public double CategoryID { get; set; }
+        public int OnSale { get; set; }
+        public int StockLevel { get; set; }
+
+    }
+}
---
+++ ASPNET/Repository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace ASPNET
+{
+    public class Repository : IProductRepository
+    {
+        private readonly IDbConnection _conn;
+        public ProductRepository(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _conn.Query<Product>("Select * FROM products;");
+        }
+
+        public IProductRepository GetProduct(int id)
+        {
+            return _conn.QuerySingle<Product>("SELECT * FROM products WHERE ProductID = @id",
+               new { id = id });
+        }
+
+        public IActionResult ViewProduct(int id)
+        {
+            var product = Repository.GetProduct(id);
+
+            return View(product);
+        }
+            
+    }
+}
